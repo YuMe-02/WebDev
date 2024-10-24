5d044a573b2764460951e8b8a2473e7ac17d8fc0
YuMe-02 <darrenyu90@gmail.com>
2024-10-23 20:57:59 -0700

    JS start

---
+++ Ch12/Calculator.js
@@ -0,0 +1,22 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function mult(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+    return operator(num1, num2);
+}
+
+var num = calculator(4,5,add);
+console.log(num);
