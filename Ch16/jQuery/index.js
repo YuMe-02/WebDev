3ab6652d4781b22edec97532d26674e504f28411
YuMe-02 <darrenyu90@gmail.com>
2024-12-04 11:38:17 -0800

    jquery mods

--- Ch16/jQuery/index.js
+++ Ch16/jQuery/index.js
@@ -1,6 +1,57 @@
+// if you want to apply and link the js and jQuery in head section
+// turns the h1 red
 
-$(document).ready(function() {
+// $(document).ready(function() {
+        
+//     $("h1").css("color", "red");
 
-    $("h1").css("color", "red");
+// });
 
+// applies new class to h1
+
+$("h1").addClass("big-title margin-50");
+
+// change text 
+$("button").text("don't click me");
+    
+// change inner html method
+$("button").html("<em> OOF <\em>");
+
+// sees value of the img
+console.log($("img").attr("src"));
+
+// use attr method to get val of href attribute
+$("a").attr("href", "http://www.yahoo.com")
+
+// add event listener to h1
+$("h1").click(function() {
+    $("h1").css("color", "purple")
 });
+
+// pressing button to change h1 to purple
+// for (var i = 0; i<5; i++) {
+//     document.querySelector("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple"
+//     });
+// }
+
+// same jquery code
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
+
+
+// key press logger
+$("input").keypress(function(event) {
+    console.log(event.key);
+})
+
+// text changer
+$("input").keypress(function(event) {
+    $("h1").text(event.key);
+})
+
+// more flexible way to take input e.g any event possible
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "green");
+})
