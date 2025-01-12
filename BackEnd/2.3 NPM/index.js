95b1a12065c3a3f123209763aaf14c620b65876e
YuMe-02 <darrenyu90@gmail.com>
2025-01-12 00:18:33 -0800

    superheroes

---
+++ BackEnd/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require("sillyname");
+import generateName from "sillyName";
+var sillyName = generateName();
+
+console.log("I am " + sillyName);
+
+// var superheroes = require("superheroes");
+// import superheroes from "superheroes";
+
+// var name = superheroes.random();
+// console.log(`I am ${name}!`);
