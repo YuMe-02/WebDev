6ec019a255c0c3790adce227b9588254782c184a
YuMe-02 <darrenyu90@gmail.com>
2025-01-17 01:02:04 -0800

    middle ware apps

--- BackEnd/3.4 Middleware/index1.js
+++ BackEnd/3.4 Middleware/index1.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -6,8 +7,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+// mounts the body parser : app.use([path], middlewareFunction);
+// gives a body to every request
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Postman Test: localhost:3000
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+// Postman Test: localhost:3000/submit
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
--- BackEnd/3.4 Middleware/solution1.js
+++ BackEnd/3.4 Middleware/solution1.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url)); // dyanically finds file path
 
 const app = express();
 const port = 3000;
@@ -11,7 +11,8 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-res.sendFile(__dirname + "/public/index.html");
+  console.log(__dirname + "/public/index.html")
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
